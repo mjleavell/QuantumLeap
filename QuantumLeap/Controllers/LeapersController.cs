c715a179da93aee320ffc012515a11d3c3caa558
Maggie Leavell <mjleavell@gmail.com>
2019-05-04 11:06:04 -0500

    getting all leapers

---
+++ QuantumLeap/Controllers/LeapersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuantumLeap.Data;
+
+namespace QuantumLeap.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeapersController : ControllerBase
+    {
+        readonly LeaperRepository _leaperRepository;
+
+        public LeapersController()
+        {
+            _leaperRepository = new LeaperRepository();
+        }
+
+        [HttpGet]
+        public ActionResult GetLeapers()
+        {
+            var allLeapers = _leaperRepository.GetLeapers();
+            return Ok(allLeapers);
+        }
+    }
+}
---
+++ QuantumLeap/Data/LeaperRepository.cs
@@ -0,0 +1,27 @@
+﻿using QuantumLeap.Models;
+using System;
+using System.Collections.Generic;
+using Dapper;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLeap.Data
+{
+    public class LeaperRepository
+    {
+        const string ConnectionString = "Server = localhost; Database = QuantumLeap; Trusted_Connection = True;";
+
+        public IEnumerable<Leaper> GetLeapers()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var sqlQuery = "SELECT * FROM Leapers";
+
+                var leapers = db.Query<Leaper>(sqlQuery);
+
+                return leapers;
+            }
+        }
+    }
+}
