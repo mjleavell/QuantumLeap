c56920de8df809126acc62a82fb9525ce29811ca
Maggie Leavell <mjleavell@gmail.com>
2019-05-04 20:33:34 -0500

    getting events

---
+++ QuantumLeap/Controllers/EventsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuantumLeap.Data;
+
+namespace QuantumLeap.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        readonly EventRepository _eventRepository;
+
+        public EventsController()
+        {
+            _eventRepository = new EventRepository();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllEvents()
+        {
+            var events = _eventRepository.GetEvents();
+
+            return Ok(events);
+        }
+    }
+}
---
+++ QuantumLeap/Data/EventRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using QuantumLeap.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace QuantumLeap.Data
+{
+    public class EventRepository
+    {
+        const string ConnectionString = "Server = localhost; Database = QuantumLeap; Trusted_Connection = True;";
+
+        public IEnumerable<Event> GetEvents()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var getQuery = "SELECT id, name, eventDate, isCorrected FROM events";
+
+                var events = db.Query<Event>(getQuery).ToList();
+
+                return events;
+            }
+        }
+
+    }
+}
